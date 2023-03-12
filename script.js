9725654e4d17e1c61e681a311a9676a3ad00e754
sadums <samjadams4@gmail.com>
2023-03-11 17:37:24 -0700

    added function to ask for password length

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Prompt user for password length
+function askLength(){
+  let lengthCorrect = false;
+  while(!lengthCorrect){
+    var length = prompt("Please enter a length between 8-128 characters");
+    // check if inputted length fits criteria. if length does not fit, will loop.
+    if(parseInt(length) > 8 && parseInt(length) < 128){
+      lengthCorrect = true;
+    }
+  }
+  return length;
+}
+
+function generatePassword(length){
+
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var length = askLength();
+
+  var password = generatePassword(length);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
