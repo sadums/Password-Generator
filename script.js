c4e3f3dc9e4fefee872f85a1b4af88fbcdfcf7e2
sadums <samjadams4@gmail.com>
2023-03-11 17:53:09 -0700

    added function to ask for character criteria

--- script.js
+++ script.js
@@ -14,20 +14,47 @@ function askLength(){
   return length;
 }
 
-function generatePassword(length){
+function askChars(){
+  let selected = false;
+  while(!selected){
+    // ask for criteria
+    var lowercase = confirm("Use lowercase alphabet characters?");
+    var uppercase = confirm("Use uppercase alphabet characters?");
+    var numeric = confirm("Use numeric characters?");
+    var special = confirm("Use special characters?");
+    // if none are selected, loop will repeat
+    if(lowercase || uppercase || numeric || special){
+      selected = true;
+    }else{
+      alert("please select at least one.")
+    }
+  }
+  // return an object containing the criteria for character selection
+  return {
+    "lowercase" : lowercase,
+    "uppercase" : uppercase,
+    "numeric" : numeric,
+    "special" : special
+  }
+}
 
+// randomly generate password based on user criteria
+function generatePassword(){
+  var length = askLength();
+  var charCriteria = askChars();
+  console.log(charCriteria);
 }
 
 // Write password to the #password input
 function writePassword() {
-  var length = askLength();
 
-  var password = generatePassword(length);
+
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
   
 }
 
-// Add event listener to generate button
+// event listener for "Generate Password" button
 generateBtn.addEventListener("click", writePassword);
